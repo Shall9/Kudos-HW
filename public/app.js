e66ea046eded17f7824382cda1e23e122d026a9c
Shall9 <hallshane856@gmail.com>
2018-12-20 15:27:27 -0500

    changes to app.js

---
+++ public/app.js
@@ -0,0 +1,11 @@
+// on page load render all kudos to list space
+// render function
+
+// on kudo button click, open our kudo modal providing to, from, body, title
+
+// once all forms are filled out, and create kudo in modal button is clicked, close modal, send new kudo to the database and re-render our list including newest Kudo.
+
+
+$(function () {
+    
+});
